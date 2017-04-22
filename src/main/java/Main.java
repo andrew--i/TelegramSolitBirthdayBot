c4ac69a4d72054ff14b986085cd1fe9262431d22
andrew--i <hiandrew807>
2017-04-22 22:08:16 +0300

    add some logic

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,12 @@
-import static spark.Spark.*;
+import static spark.Spark.get;
+import static spark.Spark.port;
 
 public class Main {
 
   public static void main(String[] args) {
 
     port(Integer.valueOf(System.getenv("PORT")));
-    staticFileLocation("/public");
-
-    get("/hello", (req, res) -> "Hello World");
-
+    get("/bot", new TelegramBotHandler());
   }
 
 }
---
+++ src/main/java/TelegramBotHandler.java
@@ -0,0 +1,21 @@
+import com.pengrad.telegrambot.BotUtils;
+import com.pengrad.telegrambot.model.Message;
+import com.pengrad.telegrambot.model.Update;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class TelegramBotHandler implements Route {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TelegramBotHandler.class);
+
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        Update update = BotUtils.parseUpdate(request.body());
+        Message message = update.message();
+        LOGGER.info("message Id: {}", message.messageId());
+        LOGGER.info("chat Id: {}", message.chat().id());
+        return "ok";
+    }
+}
