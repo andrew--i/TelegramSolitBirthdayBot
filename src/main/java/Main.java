d163572d9639e5f2ab54f8fbdc344997a31beb69
andrew--i <hiandrew807>
2017-04-22 11:43:25 +0300

    init commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import static spark.Spark.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    port(Integer.valueOf(System.getenv("PORT")));
+    staticFileLocation("/public");
+
+    get("/hello", (req, res) -> "Hello World");
+
+  }
+
+}
